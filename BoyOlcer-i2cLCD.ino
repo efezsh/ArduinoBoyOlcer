f6a14460f7f0e8cf7713ea9966d722bf9c749715
efezsh <134790448+efezsh@users.noreply.github.com>
2023-05-27 20:22:41 +0300

    Add files via upload

---
+++ BoyOlcer-i2cLCD.ino
@@ -0,0 +1,45 @@
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27,16,2);
+
+int echo = 8;
+int trig = 7;
+float sure, mesafe;
+void setup() {
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+  lcd.begin(16,2);
+  lcd.backlight();
+}
+ 
+void loop() {
+  digitalWrite(trig, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+  sure = pulseIn(echo, HIGH);
+  mesafe = sure * 0.03432 / 2;
+  lcd.setCursor(6, 0);
+  lcd.print("Boy:");
+  if (mesafe >= 199 && mesafe <= 201) {
+    lcd.setCursor(2, 1);
+    lcd.print("Dogru Mesafe");
+  } else if (200-mesafe > 0 && 200-mesafe < 201) {
+    if (200-mesafe > 99) {
+      lcd.setCursor(5, 1);
+      lcd.print(200-int(mesafe));
+      lcd.setCursor(8, 1);
+      lcd.print("cm");
+    } else if (200-mesafe < 100) {
+      lcd.setCursor(6, 1);
+      lcd.print(200-int(mesafe));
+      lcd.setCursor(8, 1);
+      lcd.print("cm");
+    }
+  } else if (200-mesafe < 0) {
+    lcd.setCursor(6,2);
+    lcd.print("Hata");
+  }
+  delay(5000);
+  lcd.clear();
+}
